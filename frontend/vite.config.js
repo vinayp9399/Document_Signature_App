16d122f553ca2002db68b7c28a7212707543dfba
Vinay Phatarpekar <vinayp9399@gmail.com>
2026-06-17 16:57:18 +0530

    backend url deployed added in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://document-signature-app-80xa.onrender.com',
         changeOrigin: true,
       },
     },
